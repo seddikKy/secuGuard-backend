f97fe220184a2269c1f434fa335c54463e896264
seddikKy <60516560+seddikKy@users.noreply.github.com>
2023-01-21 21:10:48 +0100

    Update serializers.py
    ça fonctionne mais c'est à vérifier

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from rest_framework import serializers
 
 from core.models import (Employee, Enterprise, Site, Tag, PatrolLog, Planning, Zone)
@@ -25,7 +26,7 @@ class ZoneSerializer(serializers.ModelSerializer):
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['id', 'designation','site', 'created', 'modified']
+        fields = ['id', 'designation','code_pin','site', 'created', 'modified']
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -34,14 +35,26 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ['id', 'zone','code_nfc', 'designation','order','observation', 'created', 'modified']
 
 
-class PatrolLogSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PatrolLog
-        fields = ['id', 'tag','audio_path','image_path','description_anomaly','is_checked', 'created', 'modified']
-
 
 class PlanningSerializer(serializers.ModelSerializer):
     class Meta:
         model = Planning
         fields = '__all__'
        
+
+class PatrolLogSerializer(serializers.ModelSerializer):
+    zone_id = serializers.IntegerField(source='tag.zone.id', read_only=True)
+    zone_name = serializers.CharField(source='tag.zone.designation', read_only=True)
+    tag_name = serializers.CharField(source='tag.designation', read_only=True)
+    selected_day_index = serializers.IntegerField(source='planning.selected_day_index', read_only=True)
+    tag_order = serializers.IntegerField(source='tag.order', read_only=True)
+    now = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = PatrolLog
+        fields = ['id','planning','check_datetime','check_tolerance','selected_day_index','tag','tag_name'
+        ,'tag_order','zone_id','zone_name','audio_path','image_path','description_anomaly','is_checked',
+         'created', 'modified', 'now']
+
+    def get_now(self, obj):
+        return datetime.now()
